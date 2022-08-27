17ceca7aa23d8c5ac937a7c5ebc0b83d64762943
Aman Kumar Agrawal <582002aman@gmail.com>
2022-08-27 17:40:18 +0530

    correct colors

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,27 +11,27 @@ const Header = () => {
   }, [])
 
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
-          <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
-              Blog App
-            </span>
-          </Link>
-        </div>
-        <div className="hidden md:float-left md:contents">
-          {categories.map((category) => (
-            <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                {category.name}
-              </span>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
+		<div className="container mx-auto px-10 from-primary to-secondary bg-gradient-to-tr">
+			<div className="w-full inline-block py-8">
+				<div className="md:float-left block">
+					<Link href="/">
+						<span className="cursor-pointer font-bold text-4xl text-white">
+							Blog App
+						</span>
+					</Link>
+				</div>
+				<div className="hidden md:float-left md:contents">
+					{categories.map((category) => (
+						<Link key={category.slug} href={`/category/${category.slug}`}>
+							<span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+								{category.name}
+							</span>
+						</Link>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Header
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -53,7 +53,7 @@ const PostCard = ({ post }) => {
       </p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+          <span className="transition duration-500 transform hover:-translate-y-1 hover:bg-primary-focus inline-block bg-primary text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
             Continue Reading
           </span>
         </Link>
