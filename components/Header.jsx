a76f489e0b56e46b72df7abe35ea191baf142a62
Aman Agrawal <582002aman@gmail.com>
2024-07-03 17:10:38 +0000

    Fixed Background color

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,11 +11,11 @@ const Header = () => {
   }, [])
 
   return (
-		<div className="container mx-auto px-10 from-primary to-secondary bg-gradient-to-tr">
+		<div className="container mx-auto px-10">
 			<div className="w-full inline-block py-8">
 				<div className="md:float-left block">
 					<Link href="/">
-						<span className="cursor-pointer font-bold text-4xl text-white">
+						<span className="cursor-pointer font-bold text-4xl text-black">
 							Blog App
 						</span>
 					</Link>
@@ -23,7 +23,7 @@ const Header = () => {
 				<div className="hidden md:float-left md:contents">
 					{categories.map((category) => (
 						<Link key={category.slug} href={`/category/${category.slug}`}>
-							<span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+							<span className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">
 								{category.name}
 							</span>
 						</Link>
