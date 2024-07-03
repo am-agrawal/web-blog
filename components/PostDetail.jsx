dbd8e7c45751215b9ee8632b56def5fabda77ba2
Aman Agrawal <582002aman@gmail.com>
2024-07-03 17:20:50 +0000

    Rounded bottom corners of Post feature image

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,50 +1,50 @@
-import React from "react"
-import moment from "moment"
+import React from 'react';
+import moment from 'moment';
 
 const PostDetail = ({ post }) => {
   const getContentFragment = (index, text, obj, type) => {
-    let modifiedText = text
+    let modifiedText = text;
 
     if (obj) {
       if (obj.bold) {
-        modifiedText = <b key={index}>{text}</b>
+        modifiedText = <b key={index}>{text}</b>;
       }
 
       if (obj.italic) {
-        modifiedText = <em key={index}>{text}</em>
+        modifiedText = <em key={index}>{text}</em>;
       }
 
       if (obj.underline) {
-        modifiedText = <u key={index}>{text}</u>
+        modifiedText = <u key={index}>{text}</u>;
       }
     }
 
     switch (type) {
-      case "heading-three":
+      case 'heading-three':
         return (
           <h3 key={index} className="text-xl font-semibold mb-4">
             {modifiedText.map((item, i) => (
               <React.Fragment key={i}>{item}</React.Fragment>
             ))}
           </h3>
-        )
-      case "paragraph":
+        );
+      case 'paragraph':
         return (
           <p key={index} className="mb-8">
             {modifiedText.map((item, i) => (
               <React.Fragment key={i}>{item}</React.Fragment>
             ))}
           </p>
-        )
-      case "heading-four":
+        );
+      case 'heading-four':
         return (
           <h4 key={index} className="text-md font-semibold mb-4">
             {modifiedText.map((item, i) => (
               <React.Fragment key={i}>{item}</React.Fragment>
             ))}
           </h4>
-        )
-      case "image":
+        );
+      case 'image':
         return (
           <img
             key={index}
@@ -53,11 +53,11 @@ const PostDetail = ({ post }) => {
             width={obj.width}
             src={obj.src}
           />
-        )
+        );
       default:
-        return modifiedText
+        return modifiedText;
     }
-  }
+  };
 
   return (
     <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
@@ -65,7 +65,7 @@ const PostDetail = ({ post }) => {
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className="object-top h-full w-full rounded-t-lg"
+          className="object-top h-full w-full rounded-t-lg lg:rounded-lg"
         />
       </div>
       <div className="px-4 lg:px-0">
@@ -97,19 +97,20 @@ const PostDetail = ({ post }) => {
                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
               />
             </svg>
-            <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+            <span>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
           </div>
         </div>
         <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
         {post.content.raw.children.map((typeObj, index) => {
-          const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
+          const children = typeObj.children.map((item, itemIndex) =>
+            getContentFragment(itemIndex, item.text, item)
+          );
 
-          return getContentFragment(index, children, typeObj, typeObj.type)
+          return getContentFragment(index, children, typeObj, typeObj.type);
         })}
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostDetail
+export default PostDetail;
