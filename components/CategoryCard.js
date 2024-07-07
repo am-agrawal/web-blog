341ae3c2c1a0a2e0879690ba1b8af43faaed81d8
Aman Agrawal <582002aman@gmail.com>
2024-07-07 12:42:13 +0000

    Added category page and component

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import CodeVector from '/assets/images/code.svg';
+import Link from 'next/link';
 
 const CategoryCard = ({ name, _id }) => {
   const SvgIcon = dynamic(
@@ -11,13 +12,15 @@ const CategoryCard = ({ name, _id }) => {
     }
   );
   return (
-    <div
-      key={_id}
-      className="bg-white rounded-lg p-10 h-64 w-48 flex flex-col justify-center items-start gap-8 font-semibold text-lg cursor-pointer hover:bg-violet-500 hover:text-white"
-    >
-      <SvgIcon className="w-12 h-12" />
-      <h1>{name}</h1>
-    </div>
+    <Link href={`/category/${_id}`}>
+      <div
+        key={_id}
+        className="bg-white rounded-lg p-10 h-64 w-48 flex flex-col justify-center items-start gap-8 font-semibold text-lg cursor-pointer hover:bg-violet-500 hover:text-white basis-48 shrink-0 grow-0"
+      >
+        <SvgIcon className="w-12 h-12" />
+        <h1>{name}</h1>
+      </div>
+    </Link>
   );
 };
 
--- pages/category/index.js
+++ pages/category/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import CategoryCard from './CategoryCard';
-import Link from 'next/link';
+import CategoryCard from '../../components/CategoryCard';
 
-const CategoriesCard = () => {
+const Category = () => {
   const categoriesData = [
     {
       id: 'javascript',
@@ -105,20 +102,11 @@ const CategoriesCard = () => {
   ];
 
   return (
-    <div
-      className="bg-[#EFF0F3] lg:px-32 md:px-24 px-4 py-10 flex flex-col gap-12"
-      id="categories"
-    >
-      <div className="flex justify-between text-slate-700 md:text-lg text-xs font-semibold">
-        <div>Browse The Categories &mdash;&mdash;</div>
-        <Link href='/category'>
-          <button>
-            See All Categories
-            <FontAwesomeIcon icon={faAngleRight} size="sm" className="ml-2" />
-          </button>
-        </Link>
+    <div className="bg-[#EFF0F3] lg:px-32 md:px-24 px-8 py-10 min-h-screen">
+      <div className="text-slate-700 md:text-lg text-sm font-semibold mb-12">
+        Browse The Categories &mdash;&mdash;
       </div>
-      <div className="flex gap-8 lg:w-[calc(100vw-16rem)] md:w-[calc(100vw-12rem)] w-[calc(100vw-2rem)] overflow-x-scroll">
+      <div className="md:grid flex flex-col lg:grid-cols-5 md:grid-cols-3 grid-cols-1 gap-14 items-center">
         {categoriesData.map((category) => (
           <CategoryCard
             key={category.id}
@@ -131,4 +119,4 @@ const CategoriesCard = () => {
   );
 };
 
-export default CategoriesCard;
+export default Category;
